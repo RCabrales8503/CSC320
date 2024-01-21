d551ad298e79725bc5596a71973e905520175537
RCabrales8503 <157307179+RCabrales8503@users.noreply.github.com>
2024-01-21 12:36:16 -0600

    Add files via upload

---
+++ CT1.java
@@ -0,0 +1,23 @@
+
+public class CT1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// printing out the first name
+		System.out.println("First Name: Babe");
+		
+		// printing out last name
+		System.out.println("Last Name: Ruth");
+		
+		// printing address
+		System.out.println("Street Address: 123 Main Street");
+		
+		// printing out city
+		System.out.println("City: New York");
+		
+		// printing out zip code
+		System.out.println("Zip Code: 10461");
+
+	}
+
+}
